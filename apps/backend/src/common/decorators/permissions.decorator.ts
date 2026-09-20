cbe87404c295b4a888203a851f5d8cfc4bd42b80
Javokhir Ibrokhimov <javohiribrohimov0521@gmail.com>
2026-09-20 12:31:02 +0500

    feat(auth): support any-permission route guards

--- apps/backend/src/common/decorators/permissions.decorator.ts
+++ apps/backend/src/common/decorators/permissions.decorator.ts
@@ -1,5 +1,5 @@
 import { SetMetadata } from "@nestjs/common";
 
-export const PERMISSIONS_KEY = "permissions";
+export const PERMISSIONS_KEY = "permissions";\nexport const ANY_PERMISSIONS_KEY = "permissions:any";
 
-export const Permissions = (...permissions: string[]) => SetMetadata(PERMISSIONS_KEY, permissions);
+export const Permissions = (...permissions: string[]) => SetMetadata(PERMISSIONS_KEY, permissions);\n\nexport const PermissionsAny = (...permissions: string[]) =>\n  SetMetadata(ANY_PERMISSIONS_KEY, permissions);
