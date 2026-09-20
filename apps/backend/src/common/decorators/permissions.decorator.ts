c78876fd6cc58fa73e2bb9a00d7a010601eb1092
Javokhir Ibrokhimov <javohiribrohimov0521@gmail.com>
2026-09-20 12:33:15 +0500

    fix(auth): clean any-permission decorator metadata

--- apps/backend/src/common/decorators/permissions.decorator.ts
+++ apps/backend/src/common/decorators/permissions.decorator.ts
@@ -1,5 +1,10 @@
 import { SetMetadata } from "@nestjs/common";
 
-export const PERMISSIONS_KEY = "permissions";\nexport const ANY_PERMISSIONS_KEY = "permissions:any";
+export const PERMISSIONS_KEY = "permissions";
+export const ANY_PERMISSIONS_KEY = "permissions:any";
 
-export const Permissions = (...permissions: string[]) => SetMetadata(PERMISSIONS_KEY, permissions);\n\nexport const PermissionsAny = (...permissions: string[]) =>\n  SetMetadata(ANY_PERMISSIONS_KEY, permissions);
+export const Permissions = (...permissions: string[]) =>
+  SetMetadata(PERMISSIONS_KEY, permissions);
+
+export const PermissionsAny = (...permissions: string[]) =>
+  SetMetadata(ANY_PERMISSIONS_KEY, permissions);
